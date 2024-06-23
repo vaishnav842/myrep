ca453045f4b804001f89003208cb7d3088492fc3
vaishnav842 <vaishnavkdmk842@gmail.com>
2024-06-23 10:02:52 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,20 @@
+
+function b(){
+    let ab=document.getElementById("a").value;
+    let ac=document.getElementById("b").value;
+
+    if(ab=="vaishnav" && ac=="123"){
+        if(ac=="123"){
+            alert("login successfull");
+        }
+    
+        else{
+            alert("incorrect password")
+        }
+    }
+    else {
+
+    alert("Incorrect username")}
+
+}
+
